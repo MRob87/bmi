9eb5c4665a0eb6d601ab79c0931f59287b2519a9
MRob <MRob87@gmail.com>
2016-01-17 20:29:32 -0600

    code cleanup, removing binding handler associated with input values, no longer necessary

--- bmik.php
+++ bmik.php
@@ -29,17 +29,14 @@
 			</tr>
 			<tr>
 				<td class="calculateResults">
-					<!-- <input type="text" id="currentAgeWeeks" class="smallInput" data-bind="value: currentAgeWeeks, valueUpdate: 'afterkeydown', calculateAge: 'currentAgeWeeks'"> -->
 					<b><span data-bind="text: currentAgeWeeks"></span></b>
 				</td>
 				<td>=</td>
 				<td class="calculateResults">
-					<!-- <input type="text" id="currentAgeMonths" class="smallInput" data-bind="value: currentAgeMonths, valueUpdate: 'afterkeydown', calculateAge: 'currentAgeMonths'"> -->
 					<b><span data-bind="text: currentAgeMonths"></span></b>
 				</td>
 				<td>=</td>
 				<td class="calculateResults">
-					<!-- <input type="text" id="currentAgeYears" class="smallInput" data-bind="value: currentAgeYears, valueUpdate: 'afterkeydown', calculateAge: 'currentAgeYears'"> -->
 					<b><span data-bind="text: currentAgeYears"></span></b>
 				</td>
 			</tr>
--- js/bmik.js
+++ js/bmik.js
@@ -29,16 +29,10 @@ function (ko, $) {
 		self.birthdate = ko.observable();
 
 		function monthDiff(birthDate, currentDate) {
-			var birthMonth = birthDate.getUTCMonth();
-			var birthYear = birthDate.getUTCFullYear();
-			var currentMonth = currentDate.getUTCMonth();
-			var currentYear = currentDate.getUTCFullYear();
-
 			var months;
 			var yearDiff = yeardiff(birthDate, currentDate);
 
 			months = yearDiff * 12;
-			//var days = daydiff(birthDate, currentDate) - (months * yearDiff);
 
 			return months <= 0 ? 0 : months;
 		}
@@ -59,90 +53,29 @@ function (ko, $) {
 			return daydiff(birthDate, currentDate)/365;
 		}
 
-		//self.currentAgeDays.subscribe(function(currentAgeDays) {
-		//	for (var i in keys) {
-		//		if (keys[i] != self.currentChangedValue()) {
-		//			if (keys[i] == 'currentAgeWeeks') {
-		//				$("#" + keys[i]).val((currentAgeDays/7).toFixed(6));
-		//			} else if (keys[i] == 'currentAgeMonths') {
-		//				$("#" + keys[i]).val((currentAgeDays/30).toFixed(6));
-		//			} else if (keys[i] == 'currentAgeYears') {
-		//				$("#" + keys[i]).val((currentAgeDays/365).toFixed(6));
-		//			}
-		//		}
-		//	}
-		//});
+		ko.bindingHandlers.calculateAge = {
+			update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+				// This will be called once when the binding is first applied to an element,
+				// and again whenever any observables/computeds that are accessed change
+				// Update the DOM element based on the supplied values here.
+				var key = valueAccessor();
+				var value = element.value;
+				if (key == '' || value == '' || value.indexOf("0") === 0) return true;
 
-		//ko.bindingHandlers.calculateAge = {
-		//	update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
-		//		// This will be called once when the binding is first applied to an element,
-		//		// and again whenever any observables/computeds that are accessed change
-		//		// Update the DOM element based on the supplied values here.
-		//		var key = valueAccessor();
-		//		var value = element.value;
-		//		if (key == '' || value == '' || isNaN(value)) return true;
-		//		var currentAgeDays = 0;
-		//		if (key == 'currentAgeWeeks') {
-		//			currentAgeDays = value * 7;
-		//		} else if (key == 'currentAgeMonths') {
-		//			currentAgeDays = value * 30;
-		//		} else if (key == 'currentAgeYears') {
-		//			currentAgeDays = value * 365;
-		//		}
-		//		console.log('for ' + key + ' of ' + $("#" + key).val() + ', currentAgeDays is ' + currentAgeDays);
-        //
-		//		self.currentChangedValue(key);
-		//		self.currentAgeDays(currentAgeDays);
-		//	}
-		//};
+				var currentDate = new Date();
+				var birthDate = new Date(value);
 
-			ko.bindingHandlers.calculateAge = {
-				update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
-					// This will be called once when the binding is first applied to an element,
-					// and again whenever any observables/computeds that are accessed change
-					// Update the DOM element based on the supplied values here.
-					var key = valueAccessor();
-					var value = element.value;
-					if (key == '' || value == '' || value.indexOf("0") === 0) return true;
+				var diffMonths = monthDiff(birthDate, currentDate);
+				var diffWeeks = weekdiff(birthDate, currentDate);
+				var diffDays = daydiff(birthDate, currentDate);
+				var diffYears = yeardiff(birthDate, currentDate);
+				self.currentAgeDays(diffDays.toFixed(2));
+				self.currentAgeWeeks(diffWeeks.toFixed(2));
+				self.currentAgeMonths(diffMonths.toFixed(2));
+				self.currentAgeYears(diffYears.toFixed(2));
+			}
+		};
 
-					var currentDate = new Date();
-					var birthDate = new Date(value);
-
-					var diffMonths = monthDiff(birthDate, currentDate);
-					var diffWeeks = weekdiff(birthDate, currentDate);
-					var diffDays = daydiff(birthDate, currentDate);
-					var diffYears = yeardiff(birthDate, currentDate);
-					//debugger;
-
-					//console.log('for ' + key + ' of ' + $("#" + key).val() + ', currentAgeDays is ' + currentAgeDays);
-
-					//self.currentChangedValue(key);
-					//self.currentAgeDays(currentAgeDays);
-					self.currentAgeDays(diffDays.toFixed(2));
-					self.currentAgeWeeks(diffWeeks.toFixed(2));
-					self.currentAgeMonths(diffMonths.toFixed(2));
-					self.currentAgeYears(diffYears.toFixed(2));
-				}
-			};
-
-		//this.calculateAgeEvent = function(key, data, event) {
-        //
-		//	debugger;
-		//	var currentAgeDays = 0;
-		//	if (key == 'currentAgeWeeks') {
-		//		currentAgeDays = $("#" + key).val() * 7;
-		//	} else if (key == 'currentAgeMonths') {
-		//		currentAgeDays = $("#" + key).val() * 30;
-		//	} else if (key == 'currentAgeYears') {
-		//		currentAgeDays = $("#" + key).val() * 365;
-		//	}
-		//	console.log('for ' + key + ' of ' + $("#" + key).val() + ', currentAgeDays is ' + currentAgeDays);
-        //
-		//	self.currentChangedValue(key);
-		//	self.currentAgeDays(currentAgeDays);
-        //
-		//	return true;
-		//};
 	};
 
 	ko.applyBindings(new viewModel());
